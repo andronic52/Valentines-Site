d6bf87cf6d050af75aba19aecdd7c71c9513c359
andronix <123659543+andronic52@users.noreply.github.com>
2026-01-30 17:07:42 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ const text = document.querySelector("#text");
 const vid = document.querySelector("video");
 let count = 2;
 
-// Absolute paths for GitHub Pages
+// Relative paths (works on GitHub Pages)
 const gifs = [
-  "/Valentines-Site/resources/cat-heart.gif",
-  "/Valentines-Site/resources/rusure.gif",
-  "/Valentines-Site/resources/3shocked-1.gif",
-  "/Valentines-Site/resources/4.crying.gif",
-  "/Valentines-Site/resources/5.crying.gif",
-  "/Valentines-Site/resources/idc.gif"
+  "resources/cat-heart.gif",
+  "resources/rusure.gif",
+  "resources/3shocked-1.gif",
+  "resources/4.crying.gif",
+  "resources/5.crying.gif",
+  "resources/idc.gif"
 ];
 
 // Preload GIFs
@@ -24,21 +24,21 @@ gifs.forEach(gifSrc => {
 // NO button click
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "/Valentines-Site/resources/rusure.gif";
+    gif.src = "resources/rusure.gif";
     text.innerHTML = "You meant to press YES right?🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "/Valentines-Site/resources/3shocked-1.gif";
+    gif.src = "resources/3shocked-1.gif";
     text.innerHTML = "Your hand must have slipped right?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "/Valentines-Site/resources/4.crying.gif";
+    gif.src = "resources/4.crying.gif";
     text.innerHTML = "I'm gonna cry😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -46,7 +46,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "/Valentines-Site/resources/5.crying.gif";
+    gif.src = "resources/5.crying.gif";
     text.innerHTML = "Pretty Please🥺😘";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -57,7 +57,7 @@ no.addEventListener("click", () => {
 // YES button click
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "/Valentines-Site/resources/idc.gif";
+  gif.src = "resources/idc.gif";
   text.innerHTML = "Knew it babe 😘";
   yes.innerHTML = '<a href="https://www.instagram.com/a.ndronix/">Message me</a>';
   yes.style.height = "90%";
