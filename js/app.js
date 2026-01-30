c388d97ab8f2edf062f3fc8d575707fc0f8e97d0
andronix <123659543+andronic52@users.noreply.github.com>
2026-01-30 17:04:17 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,39 +5,40 @@ const text = document.querySelector("#text");
 const vid = document.querySelector("video");
 let count = 2;
 
+// Absolute paths for GitHub Pages
 const gifs = [
-  "../resources/cat-heart.gif",
-  "../resources/rusure.gif",
-  "../resources/3shocked-1.gif",
-  "../resources/4.crying.gif",
-  "../resources/5.crying.gif",
-  "../resources/idc.gif"
+  "/Valentines-Site/resources/cat-heart.gif",
+  "/Valentines-Site/resources/rusure.gif",
+  "/Valentines-Site/resources/3shocked-1.gif",
+  "/Valentines-Site/resources/4.crying.gif",
+  "/Valentines-Site/resources/5.crying.gif",
+  "/Valentines-Site/resources/idc.gif"
 ];
 
-// to load the gifs faster
+// Preload GIFs
 gifs.forEach(gifSrc => {
   const img = new Image();
   img.src = gifSrc;
 });
 
-// changes for when NO button is clicked
+// NO button click
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "../resources/rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
+    gif.src = "/Valentines-Site/resources/rusure.gif";
     text.innerHTML = "You meant to press YES right?🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "../resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    gif.src = "/Valentines-Site/resources/3shocked-1.gif";
     text.innerHTML = "Your hand must have slipped right?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "../resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
+    gif.src = "/Valentines-Site/resources/4.crying.gif";
     text.innerHTML = "I'm gonna cry😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -45,7 +46,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "../resources/5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
+    gif.src = "/Valentines-Site/resources/5.crying.gif";
     text.innerHTML = "Pretty Please🥺😘";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -53,12 +54,12 @@ no.addEventListener("click", () => {
   }
 });
 
-// changes for when YES button is clicked
+// YES button click
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "/Valentines-Site/resources/idc.gif";
   text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href="https://www.instagram.com/a.ndronix/">Message me</a>';// inside the " " put your social media profile link
+  yes.innerHTML = '<a href="https://www.instagram.com/a.ndronix/">Message me</a>';
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
