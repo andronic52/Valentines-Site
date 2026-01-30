4746a2e2195ea0d849ff1cacdb19dd2ad7a816d0
andronix <123659543+andronic52@users.noreply.github.com>
2026-01-30 17:17:24 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,66 +3,94 @@ const no = document.querySelector("#no");
 const gif = document.querySelector("#gif");
 const text = document.querySelector("#text");
 const vid = document.querySelector("video");
+
 let count = 2;
 
-// Relative paths (works on GitHub Pages)
-const gifs = [
-  "resources/cat-heart.gif",
-  "resources/rusure.gif",
-  "resources/3shocked-1.gif",
-  "resources/4.crying.gif",
-  "resources/5.crying.gif",
-  "resources/idc.gif"
-];
+// GIF paths (make sure these exist in /resources/)
+const gifs = {
+  catHeart: "resources/cat-heart.gif",
+  rusure: "resources/rusure.gif",
+  shocked: "resources/3shocked-1.gif",
+  crying1: "resources/4.crying.gif",
+  crying2: "resources/5.crying.gif",
+  idc: "resources/idc.gif"
+};
 
-// Preload GIFs
-gifs.forEach(gifSrc => {
+// Preload GIFs and log if any fail
+Object.values(gifs).forEach(src => {
   const img = new Image();
-  img.src = gifSrc;
+  img.src = src;
+  img.onload = () => console.log(`Loaded GIF: ${src}`);
+  img.onerror = () => console.error(`Failed to load GIF: ${src}`);
 });
 
-// NO button click
+// Helper function to update GIF and text
+function updateState(gifSrc, message, yesStyles = {}, noStyles = {}) {
+  gif.src = gifSrc;
+  text.innerHTML = message;
+  
+  Object.assign(yes.style, yesStyles);
+  Object.assign(no.style, noStyles);
+}
+
+// NO button logic
 no.addEventListener("click", () => {
-  if (count == 2) {
-    gif.src = "resources/rusure.gif";
-    text.innerHTML = "You meant to press YES right?🤨";
-    yes.style.height = "65%";
-    yes.style.width = "60%";
-    no.style.width = "30%";
-    count++;
-  } else if (count == 3) {
-    gif.src = "resources/3shocked-1.gif";
-    text.innerHTML = "Your hand must have slipped right?🥹";
-    yes.style.height = "70%";
-    yes.style.width = "70%";
-    no.style.width = "20%";
-    count++;
-  } else if (count == 4) {
-    gif.src = "resources/4.crying.gif";
-    text.innerHTML = "I'm gonna cry😭";
-    yes.style.height = "80%";
-    yes.style.width = "80%";
-    no.style.fontSize = "4vh";
-    no.style.width = "10%";
-    count++;
-  } else if (count == 5) {
-    gif.src = "resources/5.crying.gif";
-    text.innerHTML = "Pretty Please🥺😘";
-    yes.style.height = "90%";
-    yes.style.width = "96%";
-    no.style.display = "none";
+  switch(count) {
+    case 2:
+      updateState(
+        gifs.rusure,
+        "You meant to press YES right?🤨",
+        {height: "65%", width: "60%"},
+        {width: "30%"}
+      );
+      count++;
+      break;
+    case 3:
+      updateState(
+        gifs.shocked,
+        "Your hand must have slipped right?🥹",
+        {height: "70%", width: "70%"},
+        {width: "20%"}
+      );
+      count++;
+      break;
+    case 4:
+      updateState(
+        gifs.crying1,
+        "I'm gonna cry😭",
+        {height: "80%", width: "80%"},
+        {width: "10%", fontSize: "4vh"}
+      );
+      count++;
+      break;
+    case 5:
+      updateState(
+        gifs.crying2,
+        "Pretty Please🥺😘",
+        {height: "90%", width: "96%"},
+        {display: "none"}
+      );
+      break;
+    default:
+      console.log("NO clicked too many times!");
   }
 });
 
-// YES button click
+// YES button logic
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "resources/idc.gif";
-  text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href="https://www.instagram.com/a.ndronix/">Message me</a>';
-  yes.style.height = "90%";
-  yes.style.width = "96%";
-  no.style.display = "none";
+
+  updateState(
+    gifs.idc,
+    "Knew it babe 😘",
+    {height: "90%", width: "96%"},
+    {display: "none"}
+  );
+
+  // Add link
+  yes.innerHTML = '<a href="https://www.instagram.com/a.ndronix/" target="_blank">Message me</a>';
+
+  // Hide video after 9s
   setTimeout(() => {
     vid.style.display = "none";
   }, 9000);
